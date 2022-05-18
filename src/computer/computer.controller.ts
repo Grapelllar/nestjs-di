cd008abb05e64398479fb4c13b715dde83688321
Grape_L <grapelllar@gmail.com>
2022-05-18 09:49:12 +0800

    di computer

---
+++ src/computer/computer.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+
+@Controller('computer')
+export class ComputerController {
+    constructor(
+        private CpuService : CpuService,
+        private diskService : DiskService
+    ){}
+
+    @Get()
+    run(){
+        return [this.CpuService.compute(1,2), this.diskService.getData()]
+    }
+}
